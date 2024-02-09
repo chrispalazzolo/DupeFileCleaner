e520d3b1f9a54b87ca380b8807249ad518e4122a
Chris Palazzolo <chrisp.developer@gmail.com>
2024-02-08 22:52:42 -0700

    Added the ability to  cancel scan and delete files.  Cleaned up styles.

--- DupeFileCleaner/MainWindow.xaml.cs
+++ DupeFileCleaner/MainWindow.xaml.cs
@@ -5,19 +5,22 @@ using System.Security.Cryptography;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
-using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Collections.ObjectModel;
 
 namespace DupeFileCleaner
 {
     public partial class MainWindow : Window
     {
         IDictionary<string, List<string>> hashedFiles = new Dictionary<string, List<string>>();
+        List<TreeViewItemModel> deleteFiles = new List<TreeViewItemModel>();
+        BackgroundWorker scanBGWorker;
 
         int fileCt = 0;
         int dirScannedCt = 0;
         int filesScannedCt = 0;
         int dupsFoundCt = 0;
+        int checkedBoxes = 0;
         
         public MainWindow()
         {
@@ -28,18 +31,24 @@ namespace DupeFileCleaner
         private void Initialize()
         {
             hashedFiles.Clear();
+            scanBGWorker = null;
+            lblScanningNow.Content = "Choose a folder and press scan to start...";
+            btnScan.IsEnabled = true;
+            btnCancelScan.IsEnabled = false;
+            tvMatches.ItemsSource = null;
+            btnDeleteFiles.IsEnabled = false;
+            deleteFiles.Clear();
+            checkedBoxes = 0;
+            lboxLogging.Items.Clear();
+            btnSaveLog.IsEnabled = false;
             fileCt = 0;
             dirScannedCt = 0;
-            filesScannedCt = 0;
-            dupsFoundCt = 0;
             lblDirScannedCt.Content = 0;
+            filesScannedCt = 0;
             lblFilesScannedCt.Content = 0;
+            dupsFoundCt = 0;
             lblDupFoundCt.Content = 0;
-            lblScanningNow.Content = "";
-            tvMatches.Items.Clear();
             prgbrScan.Value = 0;
-            lboxLogging.Items.Clear();
-            lblScanningNow.Content = "Choose a folder and press scan to start...";
         }
         private void btnFolderSelect_Click(object sender, RoutedEventArgs e)
         {
@@ -49,6 +58,7 @@ namespace DupeFileCleaner
             if (result == true)
             {
                 txtSelectedFolder.Text = dialog.FolderName;
+                Initialize();
             }
         }
 
@@ -56,40 +66,52 @@ namespace DupeFileCleaner
         {
             Initialize();
             lblScanningNow.Content = "Starting Scan...";
-
-            BackgroundWorker backgroundWorker = new BackgroundWorker();
-            backgroundWorker.WorkerReportsProgress = true;
-            backgroundWorker.WorkerSupportsCancellation = true;
-            backgroundWorker.DoWork += Worker_Scan;
-            backgroundWorker.ProgressChanged += Worker_ProgressChange;
-            backgroundWorker.RunWorkerCompleted += ScanComplete;
-            backgroundWorker.RunWorkerAsync(argument: txtSelectedFolder.Text);
+            btnCancelScan.IsEnabled = true;
+            btnScan.IsEnabled = false;
+            scanBGWorker = new BackgroundWorker();
+            scanBGWorker.WorkerReportsProgress = true;
+            scanBGWorker.WorkerSupportsCancellation = true;
+            scanBGWorker.DoWork += Worker_Scan;
+            scanBGWorker.ProgressChanged += Worker_ProgressChange;
+            scanBGWorker.RunWorkerCompleted += ScanComplete;
+            scanBGWorker.RunWorkerAsync(argument: txtSelectedFolder.Text);
         }
 
         private void Worker_Scan(object sender, DoWorkEventArgs e)
         {
-            string path = (string)e.Argument;
-            UpdateLogUI(lboxLogging, "Starting Scan :: " + path);
-            GetFileCount(path);
-            ScanDirectory(path, sender);
+            string startingDir = (string)e.Argument;
+
+            UpdateLogUI(lboxLogging, "Starting Scan :: " + startingDir);
+            GetFileCount(startingDir, e);
+            ScanDirectory(startingDir, sender, e);
         }
 
-        private void GetFileCount(string path)
+        private void GetFileCount(string dir, DoWorkEventArgs e)
         {
-            if(Directory.Exists(path))
+            string path = dir;
+
+            if (Directory.Exists(path))
             {
-                try
+                if(scanBGWorker.CancellationPending != true)
                 {
-                    fileCt += Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories).Count();
+                    try
+                    {
+                        fileCt += Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories).Count();
+                    }
+                    catch
+                    {
+                        GetFileCountPerDirectory(path, e);
+                    }
                 }
-                catch
+                else
                 {
-                    GetFileCountPerDirectory(path);
+                    e.Cancel = true;
+                    return;
                 }
             }
         }
 
-        private void GetFileCountPerDirectory(string path)
+        private void GetFileCountPerDirectory(string path, DoWorkEventArgs e)
         {
             if(Directory.Exists(path))
             {
@@ -100,13 +122,13 @@ namespace DupeFileCleaner
 
                     foreach (string dir in dirs)
                     {
-                        GetFileCount(dir);
+                        GetFileCount(dir, e);
                     }
                 }
                 catch { }
             }
         }
-        private void ScanDirectory(string path, object sender)
+        private void ScanDirectory(string path, object sender, DoWorkEventArgs doWorkEvtArgs)
         {
             if (path != String.Empty && Directory.Exists(path))
             {
@@ -116,45 +138,27 @@ namespace DupeFileCleaner
                 UpdateLogUI(lboxLogging, "Scanning Folder :: " + path);
 
                 try
-                {
+                {   
+                    ScanFiles(Directory.GetFiles(path), sender, doWorkEvtArgs);
+
                     string[] foundDirs = Directory.GetDirectories(path);
-                    string[] foundFiles = Directory.GetFiles(path);
-
-                    if (foundFiles.Length > 0)
-                    {
-                        foreach (string foundFile in foundFiles)
-                        {
-                            UpdateUI(lblScanningNow, "Scanning File: " + foundFile);
-                            UpdateLogUI(lboxLogging, "Scanning File :: " +  foundFile);
-                            string hashKey = GetFileHash(foundFile);
-                            
-                            if(hashedFiles.ContainsKey(hashKey))
-                            {
-                                hashedFiles[hashKey].Add(foundFile);
-                                dupsFoundCt++;
-                                UpdateUI(lblDupFoundCt, dupsFoundCt.ToString());
-                            }
-                            else
-                            {
-                                hashedFiles.Add(hashKey, new List<string> { foundFile });
-                            }
-
-                            filesScannedCt++;
-                            UpdateUI(lblFilesScannedCt, filesScannedCt.ToString());
-                            double prcComplete = (double)filesScannedCt / (double)fileCt * 100;
-                            (sender as BackgroundWorker).ReportProgress((int)prcComplete);
-                        }
-                    }
 
                     if (foundDirs.Length > 0)
                     {
                         foreach (string dir in foundDirs)
                         {
-                            ScanDirectory(dir, sender);
+                            if(scanBGWorker.CancellationPending != true)
+                            {
+                                ScanDirectory(dir, sender, doWorkEvtArgs);
+                            }
+                            else
+                            {
+                                return;
+                            }
                         }
                     }
                 }
-                catch(Exception)
+                catch (Exception e)
                 {
                     UpdateLogUI(lboxLogging, "Error :: Access Denied: " + path);
                 }
@@ -162,6 +166,44 @@ namespace DupeFileCleaner
             else
             {
                 UpdateLogUI(lboxLogging, "Error :: Invalid Path: " + path);
+            }
+        }
+
+        private void ScanFiles(string[] files, object sender, DoWorkEventArgs doWorkEvtArgs)
+        {
+            if (files.Length > 0)
+            {
+                foreach (string foundFile in files)
+                {
+                    if(scanBGWorker.CancellationPending != true)
+                    {
+                        UpdateUI(lblScanningNow, "Scanning File: " + foundFile);
+                        UpdateLogUI(lboxLogging, "Scanning File :: " + foundFile);
+
+                        string hashKey = GetFileHash(foundFile);
+
+                        if (hashedFiles.ContainsKey(hashKey))
+                        {
+                            hashedFiles[hashKey].Add(foundFile);
+                            dupsFoundCt++;
+                            UpdateUI(lblDupFoundCt, dupsFoundCt.ToString());
+                        }
+                        else
+                        {
+                            hashedFiles.Add(hashKey, new List<string> { foundFile });
+                        }
+
+                        filesScannedCt++;
+                        UpdateUI(lblFilesScannedCt, filesScannedCt.ToString());
+                        double prcComplete = (double)filesScannedCt / (double)fileCt * 100;
+                        (sender as BackgroundWorker).ReportProgress((int)prcComplete);
+                    }
+                    else
+                    {
+                        doWorkEvtArgs.Cancel = true;
+                        return;
+                    }
+                }
             }
         }
 
@@ -188,22 +230,6 @@ namespace DupeFileCleaner
             return hash;
         }
 
-        private TreeViewItem GetTreeNode(string name, string header)
-        {
-            TreeViewItem newNode = new TreeViewItem();
-
-            if(name != string.Empty && header != string.Empty)
-            {
-                newNode.Name = name;
-                newNode.Header = header;
-                /*Color color = new Color();
-                color. = "#FFBDBDBD";
-                newNode.Foreground = new SolidColorBrush(new Color("#FFBDBDBD"));*/
-            }
-            
-            return newNode;
-        }
-
         private void UpdateUI(ContentControl control, string value)
         {
             Dispatcher.Invoke(new Action(() => { control.Content = value; }));
@@ -221,8 +247,19 @@ namespace DupeFileCleaner
 
         private void ScanComplete(object sender, RunWorkerCompletedEventArgs e)
         {
-            lblScanningNow.Content = "Scan Complete!";
-            UpdateLogUI(lboxLogging, "Scan Complete :: Directories Scanned: " + dirScannedCt + " | Files Scanned: " + filesScannedCt + " | Duplicates Found: " + dupsFoundCt);
+            if(!e.Cancelled)
+            {
+                lblScanningNow.Content = "Scan Complete!";
+                UpdateLogUI(lboxLogging, "Scan Complete :: Directories Scanned: " + dirScannedCt + " | Files Scanned: " + filesScannedCt + " | Duplicates Found: " + dupsFoundCt);
+                btnCancelScan.IsEnabled = false;
+                btnScan.IsEnabled = true;
+                btnSaveLog.IsEnabled = true;
+            }
+            else
+            {
+                CancelScan();  
+            }
+
             FillTreeView();
         }
 
@@ -230,32 +267,58 @@ namespace DupeFileCleaner
         {
             if(hashedFiles.Count > 0)
             {
+                tvMatches.ItemsSource = null;
+                checkedBoxes = 0;
+                btnDeleteFiles.IsEnabled = false;
+
+                ObservableCollection<TreeViewItemModel> items = new ObservableCollection<TreeViewItemModel>();
+                
                 foreach(KeyValuePair<string, List<string>> kvp in hashedFiles)
                 {
                     if(kvp.Value.Count > 1)
                     {
-                        string name = "pn" + kvp.Key + "0";
-                        TreeViewItem parentNode = GetTreeNode(name, kvp.Value[0]);
+                        string name = kvp.Key;
+                        TreeViewItemModel parentNode = new TreeViewItemModel() { Name = name, Header = kvp.Value[0], Children = new ObservableCollection<TreeViewItemModel>() };
                         
                         for(int i = 1; i < kvp.Value.Count; i++)
                         {
-                            name = "sn" + kvp.Key + i;
-                            TreeViewItem subNode = GetTreeNode(name, kvp.Value[i]);
-                            parentNode.Items.Add(subNode);
+                            name = kvp.Key;
+                            parentNode.Children.Add(new TreeViewItemModel() { Name = name, Header = kvp.Value[i] });
+                            
                         }
 
-                        tvMatches.Items.Add(parentNode);
-                    } 
+                        items.Add(parentNode);
+                    }
                 }
+
+                tvMatches.ItemsSource = items;
             }
         }
 
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            btnDeleteFiles.IsEnabled = true;
+            deleteFiles.Add((TreeViewItemModel)(e.Source as CheckBox).DataContext);
+            checkedBoxes++;
+        }
+        private void CheckBox_UnChecked(object sender, RoutedEventArgs e)
+        {
+            deleteFiles.Remove((TreeViewItemModel)(e.Source as CheckBox).DataContext);
+            checkedBoxes--;
+
+            if(checkedBoxes <= 0)
+            {
+                btnDeleteFiles.IsEnabled = false;
+            }
+        }
         private void btnSaveLog_Click(object sender, RoutedEventArgs e)
         {
             if(lboxLogging.Items.Count > 0)
             {
                 SaveFileDialog saveFile = new SaveFileDialog();
                 saveFile.InitialDirectory = txtSelectedFolder.Text;
+                saveFile.Filter = "Text file (*.txt)|*.txt";
+                saveFile.DefaultExt = ".txt";
 
                 if (saveFile.ShowDialog() == true)
                 {
@@ -268,6 +331,50 @@ namespace DupeFileCleaner
 
                     File.WriteAllLines(saveFile.FileName, logItems.ToArray());
                 }
+            }
+        }
+
+        private void btnCancelScan_Click(object sender, RoutedEventArgs e)
+        {
+            scanBGWorker.CancelAsync();
+        }
+
+        private void CancelScan()
+        {
+            UpdateUI(lblScanningNow, "Scan Canceled!");
+            UpdateLogUI(lboxLogging, "Scan Canceled!");
+            btnCancelScan.IsEnabled = false;
+            btnScan.IsEnabled = true;
+        }
+
+        private void btnDeleteFiles_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Delete selected files?", "Delete Files", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Yes)
+            {
+                if(deleteFiles.Count > 0)
+                {
+                    foreach (TreeViewItemModel item in deleteFiles)
+                    {
+                        string file = item.Header;
+                        try
+                        {
+                            File.Delete(file);
+                            hashedFiles[item.Name].Remove(file);
+                            lboxLogging.Items.Add("Deleting File :: " + file);
+                        }
+                        catch
+                        {
+                            lboxLogging.Items.Add("Error :: Can't delete file: " + file);
+                        }
+                    }
+
+                    FillTreeView();
+                }
+            }
+            else
+            {
+                return;
             }
         }
     }
---
+++ DupeFileCleaner/TreeViewItemModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.ObjectModel;
+
+namespace DupeFileCleaner
+{
+    class TreeViewItemModel
+    {
+        public string Name { get; set; }
+        public string Header { get; set; }
+        public ObservableCollection<TreeViewItemModel> Children { get; set; }
+
+        public TreeViewItemModel() { }
+    }
+}
